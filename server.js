aed6f295328a84c19068b4f978329db71561432b
tom-nguyen23 <nguyent.work@gmail.com>
2023-04-13 11:05:06 +0200

    push nodejs app

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+ // server.js
+   'use strict';
+   var express = require('express');
+   var app = express();
+   app.get('/', function (req, res) {
+     res.send('Hello World!');
+   });
+   app.listen(3000, function () {
+     console.log('Example app listening on port 3000!');
+   });
