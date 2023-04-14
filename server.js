f0ed2194aa5a9b3eb1bfeeb4471c147221dff769
tom-nguyen23 <nguyent.work@gmail.com>
2023-04-14 09:24:19 +0200

    Update 140423

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
    var express = require('express');
    var app = express();
    app.get('/', function (req, res) {
-     res.send('Hello World! 130423 ! New update ! repartition node pod');
+     res.send('Hello World! New Update 14/04/2023 !');
    });
    app.listen(3000, function () {
      console.log('Example app listening on port 3000!');
